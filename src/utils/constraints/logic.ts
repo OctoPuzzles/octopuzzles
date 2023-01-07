14b5ff0caf92ba47daf557b34c2c45dacc9188ec
Martin Hunt <starwarigami@gmail.com>
2023-01-07 22:55:49 +1300

    add entropy verification

--- src/utils/constraints/logic.ts
+++ src/utils/constraints/logic.ts
@@ -14,20 +14,19 @@ export function verifyLogic(
   const negativeX = flags.indexOf('NegativeX') !== -1;
   const negativeV = flags.indexOf('NegativeV') !== -1;
   if (nonConsecutive || negativeBlack || negativeWhite || negativeX || negativeV) {
-    solution.forEach((r, i) => {
-      r.forEach((v, j) => {
+    for (let i = dimensions.margins?.top ?? 0; i < dimensions.rows - (dimensions.margins?.bottom ?? 0); ++i) {
+      for (let j = dimensions.margins?.left ?? 0; j < dimensions.columns - (dimensions.margins?.right ?? 0); ++j) {
+        const v = solution[i][j];
         if (v !== '') {
           const a = parseInt(v);
           const nbrCells = [];
           for (const step of [
-            { x: -1, y: 0 },
             { x: 1, y: 0 },
-            { x: 0, y: -1 },
             { x: 0, y: 1 }
           ]) {
             const row = i + step.x;
             const column = j + step.y;
-            if (row >= 0 && row < solution.length && column >= 0 && column < solution[0].length) {
+            if (row < dimensions.rows - (dimensions.margins?.bottom ?? 0) && column < dimensions.columns - (dimensions.margins?.right ?? 0)) {
               if (solution[row][column] !== '') {
                 nbrCells.push({ row, column });
               }
@@ -126,11 +125,58 @@ export function verifyLogic(
             );
           }
         }
-      });
-    });
+      };
+    };
   }
   if (flags.indexOf('Entropy') !== -1) {
-    //TODO
+    for (let i = dimensions.margins?.top ?? 0; i < dimensions.rows - (dimensions.margins?.bottom ?? 0) - 1; ++i) {
+      for (let j = dimensions.margins?.left ?? 0; j < dimensions.columns - (dimensions.margins?.right ?? 0) - 1; ++j) {
+        const v = solution[i][j];
+        if (v !== '') {
+          const a = parseInt(v);
+          const nbrCells = [];
+          for (const step of [
+            { x: 1, y: 0 },
+            { x: 0, y: 1 },
+            { x: 1, y: 1 }
+          ]) {
+            const row = i + step.x;
+            const column = j + step.y;
+            if (solution[row][column] !== '') {
+              nbrCells.push({ row, column });
+            }
+            else {
+              break;
+            }
+          }
+          if (nbrCells.length === 3) {
+            const b = parseInt(solution[nbrCells[0].row][nbrCells[0].column]);
+            const c = parseInt(solution[nbrCells[1].row][nbrCells[1].column]);
+            const d = parseInt(solution[nbrCells[2].row][nbrCells[2].column]);
+
+            let invalid = false;
+            if (Math.ceil(a / 3) === Math.ceil(b / 3)) {
+              invalid = Math.ceil(a / 3) === Math.ceil(c / 3) || Math.ceil(a / 3) === Math.ceil(d / 3) || Math.ceil(c / 3) === Math.ceil(d / 3);
+            }
+            else if (Math.ceil(a / 3) === Math.ceil(c / 3)) {
+              invalid = Math.ceil(a / 3) === Math.ceil(d / 3) || Math.ceil(b / 3) === Math.ceil(d / 3);
+            }
+            else if (Math.ceil(a / 3) === Math.ceil(d / 3)) {
+              invalid = Math.ceil(b / 3) === Math.ceil(c / 3);
+            }
+            else if (Math.ceil(b / 3) === Math.ceil(c / 3)) {
+              invalid = Math.ceil(b / 3) === Math.ceil(d / 3);
+            }
+
+            if (invalid) {
+              invalidCells.push(
+                ...nbrCells.filter((c) => !invalidCells.some((d) => d.row === c.row && d.column === c.column))
+              );
+            }
+          }
+        }
+      }
+    }
   }
   if (flags.indexOf('Indexed159') !== -1) {
     for (
